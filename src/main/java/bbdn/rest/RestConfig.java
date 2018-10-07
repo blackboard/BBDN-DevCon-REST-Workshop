fadafdc9f9b870554fbbbdbeba30ea1a91e8ecbf
Scott Hurrey <scott.hurrey@blackboard.com>
2018-10-07 22:11:21 +0800

    Update RestConfig

--- src/main/java/bbdn/rest/RestConfig.java
+++ src/main/java/bbdn/rest/RestConfig.java
@@ -2,9 +2,9 @@ package bbdn.rest;
 
 public abstract class RestConfig {
 
-	public static String host = "https://ultra-integ.int.bbpd.io";
-	public static String key = "d03caa33-1095-47b9-bc67-f5cd634430b1";
-	public static String secret = "QSFClAMu5KmoG8yFbHTi7pjhsseJl4uz";
+	public static String host = "";
+	public static String key = "";
+	public static String secret = "";
 	public static Boolean secure = true;
 
 }
